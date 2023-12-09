c2ea8c55c0a45473dacd74d52ba85321c7fe88a2
aammddkkzxc <ideal0427@naver.com>
2023-12-09 15:55:51 +0900

    feat: 보너스 번호 범위 유효성 검사 기능 구현

--- src/main/java/lotto/BonusNumber.java
+++ src/main/java/lotto/BonusNumber.java
@@ -1,11 +1,20 @@
 package lotto;
 
+import static lotto.Lotto.LOTTO_NUMBER_MAX_LIMIT;
+import static lotto.Lotto.LOTTO_NUMBER_MIN_LIMIT;
+
 public class BonusNumber {
     public static final String BONUS_NUMBER_RE_REQUEST_MESSAGE = "보너스 번호를 다시 입력해 주십시오.";
     private final int number;
 
     public BonusNumber(int number) {
         this.number = number;
+    }
+
+    private void validateBonusNumberRange(int number) {
+        if((number < LOTTO_NUMBER_MIN_LIMIT) || (number > LOTTO_NUMBER_MAX_LIMIT)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_RE_REQUEST_MESSAGE);
+        }
     }
 
     public int getNumber() {
