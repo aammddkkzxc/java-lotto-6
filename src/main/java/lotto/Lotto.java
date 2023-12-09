0cc4b1d37d6b9b234fc0300ff1445485fcc4343b
aammddkkzxc <ideal0427@naver.com>
2023-12-09 15:20:18 +0900

    feat: 당첨 번호 범위 유효성 검사 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,13 +5,16 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
-    public static final int LOTTO_PRICE = 1000;
     public static final String LOTTO_NUMBER_RE_REQUEST_MESSAGE = "로또 번호를 다시 입력해 주십시오.";
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_NUMBER_MAX_LIMIT = 45;
+    public static final int LOTTO_NUMBER_MIN_LIMIT = 1;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateNumbersDuplication(numbers);
+        validateNumbersRange(numbers);
 
         this.numbers = numbers;
     }
@@ -20,6 +23,14 @@ public class Lotto {
         Set<Integer> duplicationChecker = new HashSet<>(numbers);
         if (duplicationChecker.size() != numbers.size()) {
             throw new IllegalArgumentException(LOTTO_NUMBER_RE_REQUEST_MESSAGE);
+        }
+    }
+
+    private void validateNumbersRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if ((number < LOTTO_NUMBER_MIN_LIMIT) || (number > LOTTO_NUMBER_MAX_LIMIT)) {
+                throw new IllegalArgumentException(LOTTO_NUMBER_RE_REQUEST_MESSAGE);
+            }
         }
     }
 }
