2ba1aa0b6189f16a73c9116ed2485755eb4ee8fb
aammddkkzxc <ideal0427@naver.com>
2023-12-09 14:58:43 +0900

    feat: 당첨 번호 쉼표로 구분짓는 기능 구현

--- src/main/java/lotto/ui/Converter.java
+++ src/main/java/lotto/ui/Converter.java
@@ -2,12 +2,23 @@ package lotto.ui;
 
 import static lotto.PurchaseMoney.PURCHASE_MONEY_RE_REQUEST_MESSAGE;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Converter {
+    private static final String LOTTO_NUMBER_DELIMITER = ",";
+
     public static int convertPurchaseMoney(String purchaseMoney) {
         try{
             return Integer.parseInt(purchaseMoney);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(PURCHASE_MONEY_RE_REQUEST_MESSAGE);
         }
+    }
+
+    private static List<String> separateWinningLottoNumber(String winningLottoNumber) {
+        String[] separatedWinningLottoNumber = winningLottoNumber.split(LOTTO_NUMBER_DELIMITER);
+        return new ArrayList<>(Arrays.asList(separatedWinningLottoNumber));
     }
 }
