7269f85ad96b747ca7fe3c6016b1d0a1546f3e0b
aammddkkzxc <ideal0427@naver.com>
2023-12-09 14:43:36 +0900

    feat: 구입 금액 최소값 유효성 검사 기능 구현

--- src/main/java/lotto/PurchaseMoney.java
+++ src/main/java/lotto/PurchaseMoney.java
@@ -2,11 +2,20 @@ package lotto;
 
 public class PurchaseMoney {
     public static final String PURCHASE_MONEY_RE_REQUEST_MESSAGE = "구입 금액을 다시 입력해 주십시오.";
+    private static final int PURCHASE_MONEY_MINIMUM_LIMIT = 0;
 
     private final int amount;
 
     public PurchaseMoney(int amount) {
+        validatePurchaseMoneyRange(amount);
+
         this.amount = amount;
+    }
+
+    private void validatePurchaseMoneyRange(int amount) {
+        if (amount < PURCHASE_MONEY_MINIMUM_LIMIT) {
+            throw new IllegalArgumentException(PURCHASE_MONEY_RE_REQUEST_MESSAGE);
+        }
     }
 
     public int getAmount() {
