a503a486c31b2b325f7420b028ef26b7d832d718
aammddkkzxc <ideal0427@naver.com>
2023-12-09 14:48:30 +0900

    feat: 구입 금액 단위 유효성 검사 기능 구현

--- src/main/java/lotto/PurchaseMoney.java
+++ src/main/java/lotto/PurchaseMoney.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static lotto.Lotto.LOTTO_PRICE;
+
 public class PurchaseMoney {
     public static final String PURCHASE_MONEY_RE_REQUEST_MESSAGE = "구입 금액을 다시 입력해 주십시오.";
     private static final int PURCHASE_MONEY_MINIMUM_LIMIT = 0;
@@ -8,12 +10,19 @@ public class PurchaseMoney {
 
     public PurchaseMoney(int amount) {
         validatePurchaseMoneyRange(amount);
+        validatePurchaseMoneyUnit(amount);
 
         this.amount = amount;
     }
 
     private void validatePurchaseMoneyRange(int amount) {
         if (amount < PURCHASE_MONEY_MINIMUM_LIMIT) {
+            throw new IllegalArgumentException(PURCHASE_MONEY_RE_REQUEST_MESSAGE);
+        }
+    }
+
+    private void validatePurchaseMoneyUnit(int amount) {
+        if ((amount % LOTTO_PRICE) != 0) {
             throw new IllegalArgumentException(PURCHASE_MONEY_RE_REQUEST_MESSAGE);
         }
     }
