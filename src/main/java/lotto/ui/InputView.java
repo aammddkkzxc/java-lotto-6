47e87d5749696ce9850266932d154eecc23eed07
aammddkkzxc <ideal0427@naver.com>
2023-12-09 14:40:39 +0900

    feat: 구입 금액 입력 과정에서 예외 발생 시 재입력 기능 구현

--- src/main/java/lotto/ui/InputView.java
+++ src/main/java/lotto/ui/InputView.java
@@ -6,7 +6,16 @@ import lotto.PurchaseMoney;
 public class InputView {
     private static final String PURCHASE_MONEY_REQUEST_MESSAGE = "구입금액을 입력해 주세요.";
 
-    public static PurchaseMoney readPurchaseMoney() {
+    public static PurchaseMoney inputPurchaseMoney() {
+        try {
+            return readPurchaseMoney();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputPurchaseMoney();
+        }
+    }
+
+    private static PurchaseMoney readPurchaseMoney() {
         System.out.println(PURCHASE_MONEY_REQUEST_MESSAGE);
         String purchaseMoney = Console.readLine();
 
